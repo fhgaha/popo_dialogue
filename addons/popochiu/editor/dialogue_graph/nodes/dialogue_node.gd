1a407ae9fbda90cf3b419e147d15c2ad926bd092
fhgaha <antsy1992@gmail.com>
2024-06-24 02:20:40 +0300

    characters can be selected in speaker option button

--- addons/popochiu/editor/dialogue_graph/nodes/dialogue_node.gd
+++ addons/popochiu/editor/dialogue_graph/nodes/dialogue_node.gd
@@ -3,14 +3,20 @@ class_name DialogueNode extends PopoGraphNode
 
 const OPTION_SCENE = preload("res://addons/popochiu/editor/dialogue_graph/dialogue_option.tscn")
 
-@onready var options: VBoxContainer = $Options
+@onready var speaker: OptionButton = $"Slot-1/SpeakerContainer/Speaker"
+
+var options: Array[DialogueOption]:
+	get:
+		var opts: Array[DialogueOption] = []
+		opts.assign(get_children().slice(1))
+		return opts
 
 var base_color: Color = Color.WHITE
 
 func _ready() -> void:
 	type = "dialogue"
 	
-	for opt: DialogueOption in options.get_children():
+	for opt: DialogueOption in options:
 		if !opt.text_changed.is_connected(_on_option_text_changed):
 			opt.text_changed.connect(_on_option_text_changed)
 		if !opt.focus_exited.is_connected(_on_option_focus_exited):
@@ -18,22 +24,18 @@ func _ready() -> void:
 
 func add_empty_option():
 	var opt = OPTION_SCENE.instantiate()
-	options.add_child(opt)
+	add_child(opt)
 	opt.text_changed.connect(_on_option_text_changed)
 	opt.focus_exited.connect(_on_option_focus_exited)
 
 func remove_last_option():
-	remove_option(options.get_children()[-1])
+	remove_option(options[-1])
 
 func remove_option(option: DialogueOption):
-	var opts: Array[Node] = options.get_children()
-	assert(opts.size() > 1, "%s: Attempt to remove a dialogue option when there is only one option!" % self)
-	
+	assert(options.size() > 1, "%s: Attempt to remove a dialogue option when there is only one option!" % self)
 	option.queue_free()
 
 func _on_option_text_changed(new_text: String, option: DialogueOption):
-	#if !new_text.is_empty():
-		#add_empty_option()
 	pass
 
 func _on_option_focus_exited():
@@ -41,21 +43,43 @@ func _on_option_focus_exited():
 	update_slots()
 
 func update_options():
-	for opt: DialogueOption in options.get_children():
-		#print(opt.get_index(), ": ", opt.text)
+	for opt: DialogueOption in options:
 		if opt.text.is_empty():
 			opt.queue_free()
 	add_empty_option()
-	#print("----")
 
 func update_slots():
 	await get_tree().process_frame
-	var opts = options.get_children() 
-	if opts.size() == 1:
-		set_slot(opts[0].get_index(), false, 0, base_color, true, 0, base_color)
+	if options.size() == 1:
+		set_slot(options[0].get_index(), 
+			false, 0, base_color, 
+			true, 0, base_color)
 		return
 	
-	for option: DialogueOption in opts:
+	for option: DialogueOption in options:
 		var enable_right_port : bool = !option.text.is_empty()
-		set_slot(option.get_index(), false, 0, base_color, 
+		set_slot(option.get_index(), 
+			false, 0, base_color, 
 			enable_right_port, 0, base_color)
+
+func _on_speaker_pressed() -> void:
+	var path:= "res://game/characters/"
+	assert(DirAccess.dir_exists_absolute(path), "No characters created!")
+	
+	speaker.clear()
+	var dir = DirAccess.open(path)
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if dir.current_is_dir():
+			speaker.add_item(file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+
+func _on_speaker_item_selected(index: int) -> void:
+
+	
+	pass
+
+
+
