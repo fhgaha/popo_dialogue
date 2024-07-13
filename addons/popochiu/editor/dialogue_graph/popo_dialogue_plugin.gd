0745fa83799c43e43844089998496b0cd2a63cc3
fhgaha <antsy1992@gmail.com>
2024-07-14 00:31:16 +0300

    graph is a window now

--- addons/popochiu/editor/dialogue_graph/popo_dialogue_plugin.gd
+++ addons/popochiu/editor/dialogue_graph/popo_dialogue_plugin.gd
@@ -4,21 +4,33 @@ extends EditorPlugin
 const DIALOGUE_GRAPH = preload("res://addons/popochiu/editor/dialogue_graph/graph.tscn")
 
 var dialogue_graph: DialogueGraph
+var window: Window
 
 func _enter_tree() -> void:
 	dialogue_graph = DIALOGUE_GRAPH.instantiate()
-	add_control_to_bottom_panel(dialogue_graph, "Popo Dialogue")
+	window = Window.new()
+	window.popup_window = true
+	window.close_requested.connect(func(): window.hide())
+	var screen_size = DisplayServer.screen_get_size()
+	window.size = Vector2i(screen_size.x - 200, screen_size.y - 200)
+	window.keep_title_visible = true
+	window.position = Vector2i(200/2, 200/2)
+	window.hide()
+	window.add_child(dialogue_graph)
+	add_child(window)
 
 func _exit_tree() -> void:
-	if is_instance_valid(dialogue_graph):
-		remove_control_from_bottom_panel(dialogue_graph)
-		dialogue_graph.queue_free()
+	if (is_instance_valid(window)):
+		window.queue_free()
 
 func _handles(object):
 	return object is GraphData
 
 func _edit(object):
-	if object is GraphData and is_instance_valid(dialogue_graph):
+	if (object is GraphData 
+	&& is_instance_valid(dialogue_graph)
+	&& is_instance_valid(window)):
 		dialogue_graph.res_path = object.resource_path
 		dialogue_graph.load_data(object.resource_path)
-		make_bottom_panel_item_visible(dialogue_graph)
+		window.title = object.resource_name
+		window.show()
